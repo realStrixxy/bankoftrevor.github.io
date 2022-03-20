b735caf5778d37522c67b1e7e8dd7b977dcdad69
realStrixxy <47069036+Tdoescode@users.noreply.github.com>
2022-03-19 17:42:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function nameInput(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username.length > 0 && password.length > 0){
+        if(username == "username" && password == "password"){
+            alert("You will now be redirected to your account");
+            window.location.href = "index.html";
+        }
+        else{
+            alert("Username or Password Incorrect");
+        }
+    }
+    else{
+        alert("Username or Password Field Empty");
+    }
+}
